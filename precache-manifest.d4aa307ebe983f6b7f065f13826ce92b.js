37237bf5c2eea1bb873fdb6e8f346bbe97433b5a
Vadim <vadimis36@mail.ru>
2020-08-01 20:33:45 +0300

    Updates

---
+++ precache-manifest.d4aa307ebe983f6b7f065f13826ce92b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef467003956d40473bd4a6072e82e38a",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "6ba5ef744422926970e4",
+    "url": "/todoApp/static/css/3.c24c5b86.chunk.css"
+  },
+  {
+    "revision": "99e5f83097a45c1094dc",
+    "url": "/todoApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30f7ca68147c7489002b",
+    "url": "/todoApp/static/js/2.be571024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.be571024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba5ef744422926970e4",
+    "url": "/todoApp/static/js/3.e562a128.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/todoApp/static/js/3.e562a128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e5f83097a45c1094dc",
+    "url": "/todoApp/static/js/main.21231ee1.chunk.js"
+  },
+  {
+    "revision": "48232ea66735fcb6e4e6",
+    "url": "/todoApp/static/js/runtime-main.7dcaa7dc.js"
+  },
+  {
+    "revision": "a74206e479854c0ff8940d77d272a490",
+    "url": "/todoApp/static/media/preloader.a74206e4.svg"
+  },
+  {
+    "revision": "39f9f3d459cf4fab3592d8cf879212e4",
+    "url": "/todoApp/static/media/smile.39f9f3d4.svg"
+  }
+]);
